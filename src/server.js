957d539b38112490fcb05e5004161eb073ad87f0
Naldio-pluralia <naldio.pluralia@gmail.com>
2019-04-10 17:59:55 +0100

    Estrutura do Backend

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/oministack", {
+  useNewUrlParser: true
+});
+
+app.use(cors);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
+
+app.use(require("./routes"));
+
+app.listen(3333);
